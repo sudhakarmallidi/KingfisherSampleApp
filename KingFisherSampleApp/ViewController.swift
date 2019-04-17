286cc0d4eca2a0822244e9bd3ea14df9d9ac573c
Saranya Sudhakar <sudhakar.iphone@gmail.com>
2019-04-18 00:36:22 +0530

    files modified

--- KingFisherSampleApp/ViewController.swift
+++ KingFisherSampleApp/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       /* let url = URL(string: "https://www.gstatic.com/webp/gallery3/2.png")
+        let processor = DownsamplingImageProcessor(size: imageView.frame.size)
+            >> RoundCornerImageProcessor(cornerRadius: 20)
+        imageView.kf.indicatorType = .activity
+        imageView.kf.setImage(
+            with: url,
+            placeholder: UIImage(named: "placeholderImage"),
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+        {
+            result in
+            switch result {
+            case .success(let value):
+                print("Task done for: \(value.source.url?.absoluteString ?? "")")
+            case .failure(let error):
+                print("Job failed: \(error.localizedDescription)")
+            }
+        }*/
     }
 
 
